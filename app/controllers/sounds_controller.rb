699b5b2b6e02f71f34f9c7a23ed9f25c371663cd
kyle macpherson <kaimac2010@gmail.com>
2015-10-26 17:15:18 +0000

    show, index, create methods working for sound controller

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -1,6 +1,38 @@
 class SoundsController < ApplicationController
+
   def index
     @sounds = Sound.all
-    render json: @contacts, status :ok
+    render json: @sounds
   end
+
+  def create
+    @sound = Sound.new(sound_params)
+    if @sound.save
+      render json: @sound
+    else
+      render json: @sound.errors
+    end
+  end
+
+  def show
+    @sound = Sound.find(params[:id])
+    render json: @sound
+  end
+
+  # def update
+  #   if @sound.update(sound_params)
+  #     head :no_content
+  #   else
+  #     render json: @sound.errors
+  #   end
+  # end
+  #
+  # def destroy
+  #   @sound.destroy
+  # end
+
+  def sound_params
+   params.require(:sound).permit(:url)
+ end
+
 end
--- spec/sounds_feature_spec.rb
+++ spec/sounds_feature_spec.rb
@@ -1,21 +1,21 @@
-# require 'rails_helper'
-#
-# feature 'sounds' do
-#   context 'no sounds been added' do
-#     scenario 'should display a prompt to add a restaurant' do
-#       visit '/sounds'
-#       expect(page).to have_content '[]'
-#     end
-#   end
-#
-#   context 'restaurants have been added' do
-#     before do
-#       Sound.create(url: 'www.sound.com/sound')
-#     end
-#
-#     scenario 'display restaurants' do
-#       visit '/sounds'
-#       expect(page).to have_content([{url: 'www.sound.com/sound'}])
-#     end
-#   end
-# end
+require 'rails_helper'
+
+feature 'sounds' do
+  context 'no sounds been added' do
+    scenario 'should display a prompt to add a restaurant' do
+      visit '/sounds'
+      expect(page).to have_content '[]'
+    end
+  end
+
+  context 'restaurants have been added' do
+    before do
+      Sound.create(url: 'www.sound.com/sound')
+    end
+
+    # scenario 'display restaurants' do
+    #   visit '/sounds'
+    #   expect(page).to have_content([{"url\":\"www.sound.com/sound\"}])
+    # end
+  end
+end
