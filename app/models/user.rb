b7665ef6bf607c9e8452b93174c3b4297ef346dc
kyle macpherson <kaimac2010@gmail.com>
2015-10-27 21:39:59 +0000

    user has many sounds

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ require 'bcrypt'
 
 class User < ActiveRecord::Base
   include BCrypt
+  has_many :sounds
   # attr_accessible :email, :password, :password_confirmation
   has_secure_password
   validates_presence_of :password, :on => :create
