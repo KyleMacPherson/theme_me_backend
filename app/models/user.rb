c3a85732d7fe3065a314efcc3b3bd0f89f8daa67
kyle macpherson <kaimac2010@gmail.com>
2015-10-27 21:41:36 +0000

    removed redundant code in user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   include BCrypt
   has_many :sounds
-  # attr_accessible :email, :password, :password_confirmation
   has_secure_password
   validates_presence_of :password, :on => :create
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027181305) do
+ActiveRecord::Schema.define(version: 20151027213620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20151027181305) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "sounds", ["user_id"], name: "index_sounds_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -29,4 +32,5 @@ ActiveRecord::Schema.define(version: 20151027181305) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "sounds", "users"
 end
