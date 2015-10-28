5d9a4f6fa615d795b5426848fca8b09348a635e3
kyle macpherson <kaimac2010@gmail.com>
2015-10-28 08:00:18 +0000

    added sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
-  include BCrypt
-  attr_accessible :email, :password, :password_confirmation
+  has_secure_password
+  # attr_accessible :email, :password, :password_confirmation
   validates_uniqueness_of :email
   has_many :sounds
-  has_secure_password
-  validates_presence_of :password, :on => :create
-
 end
