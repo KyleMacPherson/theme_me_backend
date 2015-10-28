db5ec0d553a1dc62cf88c8565b2dcf1bbfc984fe
kyle macpherson <kaimac2010@gmail.com>
2015-10-28 07:56:36 +0000

    adder unique email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ require 'bcrypt'
 
 class User < ActiveRecord::Base
   include BCrypt
+  attr_accessible :email, :password, :password_confirmation
+  validates_uniqueness_of :email
   has_many :sounds
   has_secure_password
   validates_presence_of :password, :on => :create
